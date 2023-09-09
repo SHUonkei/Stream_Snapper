ac1a584283ffe126ae9ccdf8817b61c480491df7
unknown <koyamashu3@keio.jp>
2023-09-09 10:07:12 +0900

    add Django

--- yt_highlight_finder/yt_highlight_finder/urls.py
+++ yt_highlight_finder/yt_highlight_finder/urls.py
@@ -14,9 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
+"""_summary_
+ここで、どのurlにアクセスしたらどの処理を実行するかを管理している
+"""
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+#hello フォルダのviews.pyをhelloというなまえでimport
+import hello.views as hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+#    path('hello/', hello.index),
+    path('hello/', include('hello.urls')),
+
 ]
