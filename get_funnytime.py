341c796f3226c42cff1e925c33a6fcb9acb4b077
unknown <koyamashu3@keio.jp>
2023-09-13 19:12:47 +0900

    fix

--- get_funnytime.py
+++ get_funnytime.py
@@ -5,6 +5,9 @@ def get_chatdata(get_video_id):
 #    get_video_id="IW2t52ps27s"
     # PytchatCoreオブジェクトの取得
     livechat = pytchat.create(video_id=get_video_id)
+    print(type(livechat))
+    print(livechat.is_alive)
+    
     f = open(get_video_id+'.txt', 'x', encoding='utf-16')
     while livechat.is_alive():
 
