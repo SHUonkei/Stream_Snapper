69a0e95081ebcf99fb52603433d1cbdc22b5e10b
unknown <koyamashu3@keio.jp>
2023-09-24 18:42:29 +0900

    add read

--- RequestProcessor/main.py
+++ RequestProcessor/main.py
@@ -1,6 +1,7 @@
-import RequestProcessor.get_funnytime as get_funnytime
-import RequestProcessor.plot_chatdata as plot_chatdata
-
-get_funnytime.get_chatdata("Lo5DegTKdWY")
-print(plot_chatdata.most_funnest_time("Lo5DegTKdWY"))
+import get_funnytime as get_funnytime
+import plot_chatdata as plot_chatdata
+url = "https://www.youtube.com/watch?v=vSm-ug1ss7Y"
+videoid = url.split("=")[-1]
+get_funnytime.get_chatdata(videoid)
+print(plot_chatdata.most_funnest_time(videoid))
 
